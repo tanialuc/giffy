af844558563f74c5e7bcae0a3b56f7dd8e3d4f6e
Tania Rodríguez Rodríguez <trodriguez@bdconsultores.com>
2020-10-15 18:45:49 -0600

    Last commit of the day

--- src/components/Gif.js
+++ src/components/Gif.js
@@ -4,7 +4,7 @@ export default function Gif ({ title, id, url }) {
     return (
     <a href={`#${id}`} className = 'Gif'> 
         <h4>{title}</h4>
-        <img scr={url} />
+        <img scr={url} /> 
       </a>
     )
 }
